207c9d91c1271923302b9d8287ddcdccd6b61021
Syuji-gooner8 <135284715+Syuji-gooner8@users.noreply.github.com>
2023-06-14 12:24:23 +0000

    [start]kadai -2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,11 +12,23 @@ class BooksController < ApplicationController
   end
 
   def show
+   @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  def destroy
+    book = Book.find(params[:id])
+    book.destory
+    redirect_to books_path
+  end
   private
    def book_params
       params.require(:book).permit(:title, :body)
